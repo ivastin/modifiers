5df0c343158037187e3b8d4f231fdd483237cde4
Mladen <mladen.ivastinovic@cobeisfresh.com>
2024-09-15 07:59:42 +0200

    ios project init

---
+++ iOS/Modifiers/Modifiers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Modifiers
+//
+//  Created by Mladen Ivastinovic on 15.09.2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS/Modifiers/Modifiers/ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModifiersApp.swift
+//  Modifiers
+//
+//  Created by Mladen Ivastinovic on 15.09.2024..
+//
+
+import SwiftUI
+
+@main
+struct ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
